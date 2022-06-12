8539b72f7417aa93ec1ac566f341166f4b35a247
Abdulkadir <abinbasioglu@aof.anadolu.edu.tr>
2022-06-12 20:24:23 +0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,13 +113,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Sosyohendis Blog`,
+        short_name: `Sosyohendis`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        This will impact how browsers show your PWA/website
+		https://css-tricks.com/meta-theme-color-and-trickery/
+		theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
